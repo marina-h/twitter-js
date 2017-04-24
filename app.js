70f775a44ad0ad104774f0d7b55a31986ddbfb2c
Marina Hoashi <marinahoashi@gmail.com>
2017-04-24 17:54:55 -0400

    nodemon added

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
 const express = require( 'express' );
+const morgan = require('morgan');
 const app = express();
+
+// app.use('*', function(req, res, next) {
+//   console.log(req.method, req.originalUrl, res.statusCode)
+//   next();
+// });
+
+// app.use('/special', function(req, res, next) {
+//   console.log('You\'re very special!');
+//   next();
+// });
+
+app.use(morgan('combined'));
 
 app.get('/', function(req, res, next) {
   res.send('Welcome to our cool app!');
