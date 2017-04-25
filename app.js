cc7046282a6c46c33ffd0267c47b5cfe08daf3ac
Rachel Berkowitz <brkwtzr@gmail.com>
2017-04-25 10:52:40 -0400

    routers + non-persistent storage

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require( 'express' );
 const morgan = require('morgan');
 const nunjucks = require('nunjucks');
 const app = express();
+const routes = require('./routes');
 
 // app.use('*', function(req, res, next) {
 //   console.log(req.method, req.originalUrl, res.statusCode)
@@ -14,13 +15,14 @@ const app = express();
 // });
 
 app.use(morgan('combined'));
+app.use('/', routes);
 app.engine('html', nunjucks.render);
 app.set('view engine', 'html');
 
-app.get('/', function(req, res, next) {
-  const people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
-  res.render( 'index', {title: 'Hall of Fame', people: people} );
-});
+// app.get('/', function(req, res, next) {
+//   const people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
+//   res.render( 'index', {title: 'Hall of Fame', people: people} );
+// });
 
 app.listen(3000, function() {
   console.log('server listening');
